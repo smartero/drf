15308e8cf3a76f717a7f19f6467639ebdf896608
Olga <o.parfenova@gmail.com>
2019-11-06 23:40:55 +0300

    start a project

---
+++ core/views.py
@@ -0,0 +1,6 @@
+from django.http import JsonResponse
+from django.shortcuts import render
+
+# third party imports
+from rest_framework.response import Response
+from rest_framework.views import APIView
